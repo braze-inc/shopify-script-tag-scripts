44571409dd510b4e9cc2c59372e0f1d9c94a4231
resutovsheryan <68863412+resutovsheryan@users.noreply.github.com>
2021-11-09 10:20:58 -0500

    [PP-2187] adding expiration handling for product viewed/clicked local storage arrays (#2)

--- braze-product-script.js
+++ braze-product-script.js
@@ -35,32 +35,37 @@ function getApiUrlAndApiKeyQueryParams(){
 
 whenInitialized(function() {
 	currentPath = window.location.pathname
+	now = Date.now()
+	one_day_millis = 86400000
+					
 
 	if(currentPath.startsWith("/products/")){      
-		var alreadyClickedProducts = JSON.parse(localStorage.getItem('clicked_products') || "[]");
+		var alreadyClickedProducts = JSON.parse(localStorage.getItem('clicked_products') || "{}");
 
 		productName = currentPath.substring(10)
-		if (!alreadyClickedProducts.includes(productName)){
-			localStorage.setItem('clicked_products', JSON.stringify(alreadyClickedProducts.concat(productName)));
+		productLastClickedAt = alreadyClickedProducts[productName] || 0
+		if (now-productLastClickedAt > one_day_millis){
+			alreadyClickedProducts[productName] = now;
+			localStorage.setItem('clicked_products', JSON.stringify(alreadyClickedProducts));
 			fetchAndLogProduct(currentPath, 'shopify_product_clicked');
 		}      
 	}
 
 	if(currentPath.startsWith("/collections/")){
 		var observer = new IntersectionObserver(function(products, observer) {
-			var alreadyViewedProducts = JSON.parse(localStorage.getItem('viewed_products') || "[]");
-			var tempViewedProducts = []
+			var alreadyViewedProducts = JSON.parse(localStorage.getItem('viewed_products') || "{}");
 			for(var j = 0;j<products.length; j++){
 				if(products[j].isIntersecting === true){
 					productName = products[j].target.href.substring(products[j].target.href.indexOf("/products/")+10)
-					if(!alreadyViewedProducts.includes(productName)){
-						tempViewedProducts.push(productName);
+					productLastViewedAt = alreadyViewedProducts[productName] || 0
+					if(now-productViewedLastAt > one_day_millis){
+						alreadyViewedProducts[productName] = now;
 						fetchAndLogProduct("/products/"+productName, 'shopify_product_viewed');
 					}
 					observer.unobserve(products[j].target);
 				}
 			}
-			localStorage.setItem('viewed_products', JSON.stringify(alreadyViewedProducts.concat(tempViewedProducts)));
+			localStorage.setItem('viewed_products', JSON.stringify(alreadyViewedProducts));
 
 		}, { threshold: [1] });
 
