79fb4dafbc90f125209d82588fb1a4fe3a1f2465
Sheryan Resutov <sheryan.resutov@braze.com>
2021-12-07 08:35:43 -0500

    updating as per pr comments

--- braze-cart-script.js
+++ braze-cart-script.js
@@ -1,35 +1,34 @@
-function getApiUrlAndApiKeyQueryParams(){
-	const url = new URL([...document.getElementsByTagName("script")]
-		.map(script => script.src)
-		.find(src => src.includes("braze-cart-script"))
-	);
-	const urlSearch = new URLSearchParams(url.search)
-	return Object.fromEntries(urlSearch.entries())
-}
-
 (function(ns, fetch){
 	if(typeof fetch !== 'function') return;
+
+	function getApiUrlAndApiKeyQueryParams(){
+		const url = new URL([...document.getElementsByTagName("script")]
+			.map(script => script.src)
+			.find(src => src.includes("braze-cart-script"))
+		);
+		const urlSearch = new URLSearchParams(url.search)
+		return Object.fromEntries(urlSearch.entries())
+	}
   
-  ns.fetch = function() {
-  	if(arguments[0] == "/cart.js"){
-	    fetch.apply(this, arguments).then(response => response.json()).then(data => { 
-	    	if(data["item_count"] > 0){
-	  			const cart_cookie = data["token"];
-				const cart_storage = localStorage.getItem('cart_token');
-				const customer_id = typeof ShopifyAnalytics != "undefined" ? ShopifyAnalytics.meta.page.customerId : null // should be sending device id instead so we have cart_token->device_id
-				if(cart_cookie != null && cart_cookie != cart_storage && customer_id){
-					localStorage.setItem('cart_token', cart_cookie);
-					const queryParams = getApiUrlAndApiKeyQueryParams()
-					fetch(
-						queryParams["api_url"]+"/ecommerce/shopify/cart_update?cart_token="+cart_cookie+"&customer_id="+customer_id+"&api_key="+queryParams["api_key"]+"&shop="+queryParams["shop"], 
-						{method: 'POST'}
-					);
-				}
-	  		}
-	    });
-  	}
-    
-    return fetch.apply(this, arguments);;
-  }
-  
+  	ns.fetch = function() {
+	  	if(arguments[0] == "/cart.js"){
+		    fetch.apply(this, arguments).then(response => response.json()).then(data => { 
+		    	if(data["item_count"] > 0){
+		  			const cart_cookie = data["token"];
+					const cart_storage = localStorage.getItem('cart_token');
+					appboy.getDeviceId(function(device_id) {
+   						if(cart_cookie != null && cart_cookie != cart_storage){
+							localStorage.setItem('cart_token', cart_cookie);
+							const queryParams = getApiUrlAndApiKeyQueryParams()
+							fetch(
+								queryParams["api_url"]+`/ecommerce/shopify/cart_update?cart_token=${cart_cookie}&device_id=${device_id}&api_key=${queryParams["api_key"]}&shop=${queryParams["shop"]}`, 
+								{method: 'POST'}
+							);
+						}
+					});					
+		  		}
+		    });
+	  	}
+	    return fetch.apply(this, arguments);;
+  	}	
 }(window, window.fetch));
--- braze-product-script.js
+++ braze-product-script.js
@@ -1,77 +1,77 @@
-function whenInitialized(callback) {
-	const interval = 100;
-	window.setTimeout(function() {
-		if (typeof appboy != "undefined" && appboy.getUser()) {
-			callback();
-		} else {
-			whenInitialized(callback);
-		}
-	}, interval);
-}
-
-function fetchAndLogProduct(productPath, event){
-	fetch(productPath+".js")
-	  .then(response => response.json())
-	  .then(data => {
-	  		console.log(productPath+" "+event+" "+data);
-	      	appboy.logCustomEvent(event, {
-	      		'id': data.id,
-	      		'title': data.title,
-	      		'price': data.price,
-	      		'vendor': data.vendor,
-	      		'images': data.images
-	      	});
-    });
-}
-
-function getApiUrlAndApiKeyQueryParams(){
-	const url = new URL([...document.getElementsByTagName("script")]
-		.map(script => script.src)
-		.find(src => src.includes("braze-product-script"))
-	);
-	const urlSearch = new URLSearchParams(url.search)
-	return Object.fromEntries(urlSearch.entries())
-}
-
-whenInitialized(function() {
-	currentPath = window.location.pathname
-	now = Date.now()
-	one_day_millis = 86400000
-					
-
-	if(currentPath.startsWith("/products/")){      
-		var alreadyClickedProducts = JSON.parse(localStorage.getItem('clicked_products') || "{}");
-
-		productName = currentPath.substring(10)
-		productLastClickedAt = alreadyClickedProducts[productName] || 0
-		if (now-productLastClickedAt > one_day_millis){
-			alreadyClickedProducts[productName] = now;
-			localStorage.setItem('clicked_products', JSON.stringify(alreadyClickedProducts));
-			fetchAndLogProduct(currentPath, 'shopify_product_clicked');
-		}      
-	}
-
-	if(currentPath.startsWith("/collections/")){
-		var observer = new IntersectionObserver(function(products, observer) {
-			var alreadyViewedProducts = JSON.parse(localStorage.getItem('viewed_products') || "{}");
-			for(var j = 0;j<products.length; j++){
-				if(products[j].isIntersecting === true){
-					productName = products[j].target.href.substring(products[j].target.href.indexOf("/products/")+10)
-					productLastViewedAt = alreadyViewedProducts[productName] || 0
-					if(now-productLastViewedAt > one_day_millis){
-						alreadyViewedProducts[productName] = now;
-						fetchAndLogProduct("/products/"+productName, 'shopify_product_viewed');
-					}
-					observer.unobserve(products[j].target);
-				}
+(function(){
+	function whenInitialized(callback) {
+		const interval = 100;
+		window.setTimeout(function() {
+			if (typeof appboy != "undefined" && appboy.getUser()) {
+				callback();
+			} else {
+				whenInitialized(callback);
 			}
-			localStorage.setItem('viewed_products', JSON.stringify(alreadyViewedProducts));
-
-		}, { threshold: [1] });
-
-		products = document.querySelectorAll("[href*='/products/']");
-		for(var i=0;i<products.length;i++){
-			observer.observe(products[i]);
-		}
+		}, interval);
 	}
+
+	function fetchAndLogProduct(productPath, event){
+		fetch(productPath+".js")
+		  .then(response => response.json())
+		  .then(data => {
+		      	appboy.logCustomEvent(event, {
+		      		'id': data.id,
+		      		'title': data.title,
+		      		'price': data.price,
+		      		'vendor': data.vendor,
+		      		'images': data.images
+		      	});
+	    });
+	}
+
+	function getApiUrlAndApiKeyQueryParams(){
+		const url = new URL([...document.getElementsByTagName("script")]
+			.map(script => script.src)
+			.find(src => src.includes("braze-product-script"))
+		);
+		const urlSearch = new URLSearchParams(url.search)
+		return Object.fromEntries(urlSearch.entries())
+	}
+
+	whenInitialized(function() {
+		currentPath = window.location.pathname
+		now = Date.now()
+		one_day_millis = 86400000
+						
+
+		if(currentPath.startsWith("/products/")){      
+			const alreadyClickedProducts = JSON.parse(localStorage.getItem('clicked_products') || "{}");
+
+			productName = currentPath.substring(10)
+			productLastClickedAt = alreadyClickedProducts[productName] || 0
+			if (now-productLastClickedAt > one_day_millis){
+				alreadyClickedProducts[productName] = now;
+				localStorage.setItem('clicked_products', JSON.stringify(alreadyClickedProducts));
+				fetchAndLogProduct(currentPath, 'shopify_product_clicked');
+			}      
+		}
+		if(currentPath.startsWith("/collections/")){
+			const observer = new IntersectionObserver(function(products, observer) {
+				const alreadyViewedProducts = JSON.parse(localStorage.getItem('viewed_products') || "{}");
+				for(const product of products){
+					if(product.isIntersecting === true){
+						productName = product.target.href.substring(product.target.href.indexOf("/products/")+10)
+						productLastViewedAt = alreadyViewedProducts[productName] || 0
+						if(now-productLastViewedAt > one_day_millis){
+							alreadyViewedProducts[productName] = now;
+							fetchAndLogProduct("/products/"+productName, 'shopify_product_viewed');
+						}
+						observer.unobserve(product.target);
+					}
+				}
+				localStorage.setItem('viewed_products', JSON.stringify(alreadyViewedProducts));
+
+			}, { threshold: [1] });
+
+			products = document.querySelectorAll("[href*='/products/']");
+			for(const product of products){
+				observer.observe(product);
+			}
+		}
+	});
 });
