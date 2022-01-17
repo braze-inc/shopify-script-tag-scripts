cb2146a386be9c963c25cde27acf1a126a2d397e
resutovsheryan <68863412+resutovsheryan@users.noreply.github.com>
2022-01-17 13:16:57 -0500

    Handle newer store cart add (#4)

--- braze-cart-script.js
+++ braze-cart-script.js
@@ -9,6 +9,17 @@
 		const urlSearch = new URLSearchParams(url.search)
 		return Object.fromEntries(urlSearch.entries())
 	}
+
+	function readCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
   
   	ns.fetch = function() {
 	  	if(arguments[0] == "/cart.js"){
@@ -28,6 +39,20 @@
 					});					
 		  		}
 		    });
+	  	}
+	  	else if (arguments[0] == "/cart/add"){
+	  		const cart_cookie = readCookie("cart");
+			const cart_storage = localStorage.getItem('cart_token');
+			appboy.getDeviceId(function(device_id) {
+				if(cart_cookie != null && cart_cookie != cart_storage){
+					localStorage.setItem('cart_token', cart_cookie);
+					const queryParams = getApiUrlAndApiKeyQueryParams()
+					fetch(
+						'https://'+queryParams["sdk_url"]+`/api/v3/shopify/cart_update?cart_token=${cart_cookie}&device_id=${device_id}&api_key=${queryParams["api_key"]}&shop=${queryParams["shop"]}`, 
+						{method: 'POST'}
+					);
+				}
+			});	
 	  	}
 	    return fetch.apply(this, arguments);;
   	}	
